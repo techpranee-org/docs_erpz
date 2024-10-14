dd40ba5dca9871ede4b8d1139df5bd72ff4de1c0
LORD-JINXXX <mustafamdsajid1@gmail.com>
2024-10-14 18:06:14 +0530

    Added project, stock paged and implemented edit page, git icon and search bar properly

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-// _app.js
-import '../app/globals.css'; // Adjust the path based on where global.css is located
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
